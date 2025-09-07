495ac2bbb15b0fcdf2d0943c32667d36d67482be
Renan Domingos <rna.domingos@hotmail.com>
2025-09-07 09:50:01 -0300

    feat: add Account User model

---
+++ src/data/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Boolean, Integer, String
+from src.infra.database import Base
+
+class User(Base):
+
+    __tablename__ = "account_users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    role = Column(String, nullable=True)
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
