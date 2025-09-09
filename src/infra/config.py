025fea5e7fec631f9b6fba324fef4989cdaf06fd
Renan Domingos <rna.domingos@hotmail.com>
2025-09-09 17:53:22 -0300

    feat: add oracle settings

--- src/infra/config.py
+++ src/infra/config.py
@@ -15,12 +15,23 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 5    
     DOMAIN: str = "localhost"
 
+    # Postgres Conn
     POSTGRES_SCHEMA: str = "postgresql+psycopg"
     POSTGRES_USER: str 
     POSTGRES_PASSWORD: str 
     POSTGRES_SERVER: str 
     POSTGRES_PORT: int = 5432 
     POSTGRES_DB: str     
+
+    # Oracle Conn
+    ORACLE_USERNAME: str
+    ORACLE_PASSWORD: str
+    ORACLE_HOST: str
+    ORACLE_PORT: int
+    ORACLE_SERVICE: str
+    ORACLE_DRIVER: str
+
+
 
     @computed_field
     @property
@@ -30,7 +41,7 @@ class Settings(BaseSettings):
     @computed_field
     @property
     def SQLALCHEMY_POSTGRES_URL(self) -> str | PostgresDsn:
-        return MultiHostUrl.build(
+        return  MultiHostUrl.build(
             scheme=self.POSTGRES_SCHEMA,
             username=self.POSTGRES_USER,
             password=self.POSTGRES_PASSWORD,
@@ -38,5 +49,14 @@ class Settings(BaseSettings):
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB
         ) # type: ignore
+    
 
-settings = Settings() # type: ignore    
+    @computed_field
+    @property
+    def SQLALCHEMY_ORACLE_URL(self) -> str:
+        return f"oracle+oracledb://{self.ORACLE_USERNAME}:{self.ORACLE_PASSWORD}@{self.ORACLE_HOST}:{self.ORACLE_PORT}/?service_name={self.ORACLE_SERVICE}"
+    
+    @computed_field
+    @property
+    def SQLALCHEMY_ORACLE_DRIVER(self) -> str:
+        return f"{self.ORACLE_DRIVER}"
