4e353dd5696d85b8c75d5aa0d740542fe8f988b5
Renan Domingos <rna.domingos@hotmail.com>
2025-09-10 10:06:41 -0300

    feat: config oracle connection

--- src/infra/databases/oracle_nbs.py
+++ src/infra/databases/oracle_nbs.py
@@ -1,11 +1,14 @@
+from typing import Annotated
+
 import oracledb
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from fastapi import Depends
+
 
 from src.infra.config import settings
 
-
-oracledb.init_oracle_client(lib_dir=f"{settings.SQLALCHEMY_ORACLE_DRIVER}")
+oracledb.init_oracle_client(lib_dir=f"{settings.ORACLE_DRIVER}")
 
 SQLALCHEMY_ORACLE_URL = settings.SQLALCHEMY_ORACLE_URL
 
@@ -15,3 +18,11 @@ engine = create_engine(
 
 sessionOracleLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
+def get_oracle_db():
+    db = sessionOracleLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+SessionOra = Annotated[Session, Depends(get_oracle_db)]
