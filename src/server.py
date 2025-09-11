a0bbe78e190ddf94849c11629b75d850a57509c7
Renan Domingos <rna.domingos@hotmail.com>
2025-09-11 18:54:37 -0300

    refactor: add fastapi params config

--- src/server.py
+++ src/server.py
@@ -2,10 +2,24 @@ from fastapi import FastAPI
 from src.infra.databases.postgres import Base, engine 
 from src.routes import auth_router, nbs_router
 
-
 Base.metadata.create_all(bind=engine)
 
+app = FastAPI(
+    title="API Grand Brasil 🚗",
+    description="Esta API retorna informações de veículos, entregas e agendamentos de serviço.",
+    version="1.0.0",
+    servers=[
+        {"url": "http://127.0.0.1:8050", "description": "Servidor Dev"},
+        {"url": "http://192.168.16.28:8050", "description": "Servidor Local"},
+        {"url": "http://api.grandbrasil.com.br:8050", "description": "Servidor Produção"},
+    ],
+    contact={
+        "name": "Tiveo Tecnologia",
+        "url": "https://tiveo.com.br",
+        "email": "suporte@tiveo.com.br",
+    },
+    
+)
 
-app = FastAPI()
 app.include_router(auth_router.router)
 app.include_router(nbs_router.nbs_router)
