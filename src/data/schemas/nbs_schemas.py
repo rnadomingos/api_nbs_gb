8d30ef6f6c860fa120430890850451c25f808cf0
Renan Domingos <rna.domingos@hotmail.com>
2025-09-12 17:11:04 -0300

    refactor: update schema VehicleSoldResponse return string

--- src/data/schemas/nbs_schemas.py
+++ src/data/schemas/nbs_schemas.py
@@ -19,7 +19,7 @@ class VehicleRequest(BaseModel):
     )
            
 class VehicleSoldResponse(BaseModel):
-    vendido: bool
+    vendido: str
 
     model_config = ConfigDict(
         from_attributes=True,
